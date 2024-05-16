9a3593ac1c0b7b2ed2b780d7f7b8f72afdfc2358
Leandro Calvetti <lcalvetti1@gmail.com>
2024-05-16 19:14:04 -0300

    cambios en config

--- config/config.php
+++ config/config.php
@@ -1,11 +1,12 @@
 <?php
 // config.php
+
+define('URL_PREFIX', '/mvccomposer/');
+define('URL_BASE_PATH', 'http://' . $_SERVER['HTTP_HOST'] . URL_PREFIX);
+define('URL_BASE', URL_BASE_PATH . '/index.php?r=');
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');  // Puerto estándar de MySQL, cambia según necesidades
 define('DB_NAME', 'automotora');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_CHARSET', 'utf8mb4');
-define('URL_PREFIX', '/mvccomposer/');
-define('URL_BASE_PATH', 'http://localhost/mvccomposer/');
-define('URL_BASE', 'http://localhost/mvccomposer/index.php?r=');
